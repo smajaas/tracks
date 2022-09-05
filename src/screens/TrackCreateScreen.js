d6c276ec6f9183301401c946e4ca2de9b18c44a8
smajaas <smajaas@gmail.com>
2022-09-06 00:28:29 +0530

    final commits

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -8,6 +8,7 @@ import Map from "../components/Map";
 import { Context as LocationContext } from "../context/LocationContext";
 import useLocation from "../hooks/useLocation";
 import TrackForm from "../components/TrackForm";
+import { FontAwesome } from '@expo/vector-icons';
 
 const TrackCreateScreen = ({ isFocused }) => {
   const { state: { recording}, addLocation } = useContext(LocationContext);
@@ -28,6 +29,12 @@ const TrackCreateScreen = ({ isFocused }) => {
     </SafeAreaView>
   );
 };
+
+TrackCreateScreen.navigationOptions = {
+  title: 'Add Track',
+  tabBarIcon: <FontAwesome name="plus" size={20} />
+};
+
 
 const styles = StyleSheet.create({});
 
